d31d4df7d0bab15599258f6fa111bb0a5eec5bcc
script-js <134544514+script-js@users.noreply.github.com>
2024-03-21 09:25:14 -0700

    Update import.js

--- import.js
+++ import.js
@@ -1,6 +1,10 @@
-function analyticsInit(id,apiKey) {
+function analyticsInit(id,apiKey,logLoc) {
   var anframe = document.createElement("iframe")
   anframe.style = "display:none";
-  anframe.src = "https://exactalytics.pages.dev/log?metaID=" + id + "&metaPage=" + location.href.split(location.host)[1].split("?")[0].split("#")[0] + "&key=" + apiKey;
+  if (logLoc) {
+    anframe.src = logLoc + "?metaID=" + id + "&metaPage=" + location.href.split(location.host)[1].split("?")[0].split("#")[0] + "&key=" + apiKey;
+  } else {
+    anframe.src = "https://exactalytics.pages.dev/log?metaID=" + id + "&metaPage=" + location.href.split(location.host)[1].split("?")[0].split("#")[0] + "&key=" + apiKey;
+  }
   document.documentElement.appendChild(anframe)
 }
